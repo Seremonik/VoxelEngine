064992acbfa5fecd76d7b617d24f6bc399a2d04c
Michal Duziak <m.duziak@lucidrealitylabs.com>
2024-08-09 21:57:30 +0200

    Added data Encoding

--- Assets/VoxelEngine/Runtime/ChunkMeshGenerator.cs
+++ Assets/VoxelEngine/Runtime/ChunkMeshGenerator.cs
@@ -14,6 +14,7 @@ namespace VoxelEngine
         public float3 pos;
         public int voxelId;
         public int faceIndex;
+        public int data;
     }
 
     [BurstCompile]
@@ -35,42 +36,45 @@ namespace VoxelEngine
                 {
                     for (int z = 0; z < 32; z++)
                     {
-                        if (isAir(x, y, z, Voxels))
+                        int arrayIndex = x + y * 32 + z * 32 * 32;
+                        ushort voxel = Voxels[arrayIndex];
+                        
+                        if (isAir(arrayIndex, Voxels))
                             continue;
-
-                        if (isAir(x - 1, y, z, Voxels))
-                        {
-                            GenerateFace(x, y, z, 0, Triangles, Verts, vertexCount);
-                            vertexCount += 4;
-                        }
 
                         if (isAir(x + 1, y, z, Voxels))
                         {
-                            GenerateFace(x + 1, y, z, 1, Triangles, Verts, vertexCount);
+                            GenerateFace(x + 1, y, z, 1, Triangles, Verts, voxel, vertexCount);
+                            vertexCount += 4;
+                        }
+
+                        if (isAir(x - 1, y, z, Voxels))
+                        {
+                            GenerateFace(x, y, z, 0, Triangles, Verts, voxel,vertexCount);
                             vertexCount += 4;
                         }
 
                         if (isAir(x, y + 1, z, Voxels))
                         {
-                            GenerateFace(x, y + 1, z, 2, Triangles, Verts, vertexCount);
+                            GenerateFace(x, y + 1, z, 2, Triangles, Verts, voxel,vertexCount);
                             vertexCount += 4;
                         }
 
                         if (isAir(x, y - 1, z, Voxels))
                         {
-                            GenerateFace(x, y, z, 3, Triangles, Verts, vertexCount);
+                            GenerateFace(x, y, z, 3, Triangles, Verts, voxel,vertexCount);
                             vertexCount += 4;
                         }
 
                         if (isAir(x, y, z + 1, Voxels))
                         {
-                            GenerateFace(x, y, z + 1, 4, Triangles, Verts, vertexCount);
+                            GenerateFace(x, y, z + 1, 4, Triangles, Verts, voxel,vertexCount);
                             vertexCount += 4;
                         }
 
                         if (isAir(x, y, z - 1, Voxels))
                         {
-                            GenerateFace(x, y, z, 5, Triangles, Verts, vertexCount);
+                            GenerateFace(x, y, z, 5, Triangles, Verts, voxel,vertexCount);
                             vertexCount += 4;
                         }
                     }
@@ -90,10 +94,57 @@ namespace VoxelEngine
 
             return voxels[x + y * 32 + z * 32 * 32] == 0;
         }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool isAir(int arrayIndex, NativeArray<ushort> voxels)
+        {
+            return voxels[arrayIndex] == 0;
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void FrontFace(NativeList<int> Triangles, int vertexCount)
+        {
+            //Check the AddRange performance
+            Triangles.Add(vertexCount);
+            Triangles.Add(vertexCount + 1);
+            Triangles.Add(vertexCount + 2);
+            Triangles.Add(vertexCount);
+            Triangles.Add(vertexCount + 2);
+            Triangles.Add(vertexCount + 3);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void BackFace(NativeList<int> Triangles, int vertexCount)
+        {
+            Triangles.Add(vertexCount);
+            Triangles.Add(vertexCount + 2);
+            Triangles.Add(vertexCount + 1);
+            Triangles.Add(vertexCount);
+            Triangles.Add(vertexCount + 3);
+            Triangles.Add(vertexCount + 2);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int EncodeValue(int faceId, ushort voxelId, int x, int y, int z)
+        {
+            //change to byte
+            int result = faceId;
+            result <<= 8;
+            result |= voxelId;
+            result <<= 6;
+            result |= x;
+            result <<= 6;
+            result |= y;
+            result <<= 6;
+            result |= z;
+
+            return result;
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void GenerateFace(int x, int y, int z, int face, NativeList<int> Triangles,
-            NativeList<Vertex> Verts, int vertexCount)
+            NativeList<Vertex> Verts, ushort voxelId, int vertexCount)
         {
             switch (face)
             {
@@ -102,59 +153,57 @@ namespace VoxelEngine
                     {
                         pos = new float3(x, y, z),
                         faceIndex = 1,
+                        data = EncodeValue(1, voxelId, x, y, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z + 1),
                         faceIndex = 1,
+                        data = EncodeValue(1, voxelId, x, y, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z + 1),
                         faceIndex = 1,
+                        data = EncodeValue(1, voxelId, x, y + 1, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z),
                         faceIndex = 1,
+                        data = EncodeValue(1, voxelId, x, y + 1, z),
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 3);
+                    FrontFace(Triangles, vertexCount);
                     break;
 
                 case 1: //Right
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z),
+                        data = EncodeValue(0, voxelId, x, y, z),
                         faceIndex = 0,
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z + 1),
+                        data = EncodeValue(0, voxelId, x, y, z + 1),
                         faceIndex = 0,
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z + 1),
+                        data = EncodeValue(0, voxelId, x, y + 1, +1),
                         faceIndex = 0,
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z),
+                        data = EncodeValue(0, voxelId, x, y + 1, z),
                         faceIndex = 0,
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 3);
-                    Triangles.Add(vertexCount + 2);
+                    BackFace(Triangles, vertexCount);
 
                     break;
                 case 2: //Top
@@ -162,59 +211,56 @@ namespace VoxelEngine
                     {
                         pos = new float3(x, y, z),
                         faceIndex = 2,
+                        data = EncodeValue(2, voxelId, x, y, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z + 1),
                         faceIndex = 2,
+                        data = EncodeValue(2, voxelId, x, y, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z + 1),
                         faceIndex = 2,
+                        data = EncodeValue(2, voxelId, x + 1, y, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z),
                         faceIndex = 2,
+                        data = EncodeValue(2, voxelId, x + 1, y, z),
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 3);
-
+                    FrontFace(Triangles, vertexCount);
                     break;
                 case 3: //Bottom
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z),
                         faceIndex = 3,
+                        data = EncodeValue(3, voxelId, x, y, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z + 1),
                         faceIndex = 3,
+                        data = EncodeValue(3, voxelId, x, y, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z + 1),
                         faceIndex = 3,
+                        data = EncodeValue(3, voxelId, x + 1, y, z + 1),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z),
                         faceIndex = 3,
+                        data = EncodeValue(3, voxelId, x + 1, y, z),
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 3);
-                    Triangles.Add(vertexCount + 2);
+                    BackFace(Triangles, vertexCount);
 
                     break;
                 case 4: //Bottom
@@ -222,59 +268,56 @@ namespace VoxelEngine
                     {
                         pos = new float3(x, y, z),
                         faceIndex = 4,
+                        data = EncodeValue(4, voxelId, x, y, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z),
                         faceIndex = 4,
+                        data = EncodeValue(4, voxelId, x, y + 1, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y + 1, z),
                         faceIndex = 4,
+                        data = EncodeValue(4, voxelId, x + 1, y + 1, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z),
                         faceIndex = 4,
+                        data = EncodeValue(4, voxelId, x + 1, y, z),
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 3);
-                    Triangles.Add(vertexCount + 2);
+                    BackFace(Triangles, vertexCount);
                     break;
                 case 5: //Bottom
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y, z),
                         faceIndex = 5,
+                        data = EncodeValue(5, voxelId, x, y, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x, y + 1, z),
                         faceIndex = 5,
+                        data = EncodeValue(5, voxelId, x, y + 1, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y + 1, z),
                         faceIndex = 5,
+                        data = EncodeValue(5, voxelId, x + 1, y + 1, z),
                     });
                     Verts.Add(new Vertex
                     {
                         pos = new float3(x + 1, y, z),
                         faceIndex = 5,
+                        data = EncodeValue(5, voxelId, x + 1, y, z),
                     });
 
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 1);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount);
-                    Triangles.Add(vertexCount + 2);
-                    Triangles.Add(vertexCount + 3);
-
+                    FrontFace(Triangles, vertexCount);
                     break;
             }
         }
@@ -310,6 +353,7 @@ namespace VoxelEngine
                 new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
                 new VertexAttributeDescriptor(VertexAttribute.TexCoord1, VertexAttributeFormat.UInt32, 1),
                 new VertexAttributeDescriptor(VertexAttribute.TexCoord2, VertexAttributeFormat.UInt32, 1),
+                new VertexAttributeDescriptor(VertexAttribute.TexCoord3, VertexAttributeFormat.UInt32, 1),
             };
             var vertexCount = verts.Length;
             var trisCount = triangles.Length;
